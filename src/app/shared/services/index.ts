7c7a28427b590d9f34f0709efa7a685fdf96ed90
tarlepp <tarmo.leppanen@protacon.com>
2017-03-02 18:47:45 +0200

    feat: Added player service

--- src/app/shared/services/index.ts
+++ src/app/shared/services/index.ts
@@ -1,13 +1,16 @@
 import { MessageService } from './message.service';
+import { PlayerService } from './player.service';
 import { VideoQueueService } from './video-queue.service';
 import { YoutubeApiService } from './youtube-api.service';
 
 export * from './message.service';
+export * from './player.service';
 export * from './video-queue.service';
 export * from './youtube-api.service';
 
 export const Services = [
   MessageService,
+  PlayerService,
   VideoQueueService,
   YoutubeApiService,
 ];
---
+++ src/app/shared/services/player.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+
+@Injectable()
+export class PlayerService {
+  public state$: ReplaySubject<YT.PlayerState> = new ReplaySubject(1);
+  public ready$: ReplaySubject<boolean> = new ReplaySubject(1);
+  private player: YT.Player;
+  private state: YT.PlayerState;
+  private ready = false;
+
+  /**
+   * Constructor of the class
+   */
+  public constructor() { }
+
+  /**
+   * Setter for YouTube player, this will also set ready bit to true.
+   *
+   * @param {YT.Player} player
+   */
+  public setPlayer(player: YT.Player) {
+    this.player = player;
+
+    this.ready = true;
+    this.ready$.next(this.ready);
+  }
+
+  /**
+   * Getter for YouTube player
+   *
+   * @returns {YT.Player}
+   */
+  public getPlayer(): YT.Player {
+    return this.player;
+  }
+
+  /**
+   * Method to chang
+   *
+   * @param {YT.PlayerState}  state
+   * @returns {ReplaySubject<YT.PlayerState>}
+   */
+  public changeState(state: YT.PlayerState): ReplaySubject<YT.PlayerState> {
+    this.state = state;
+    this.state$.next(this.state);
+
+    return this.state$;
+  }
+
+  /**
+   * Method to start playing video
+   */
+  public playVideo() {
+    this.ready ? this.player.playVideo() : (() => { })();
+  }
+
+  /**
+   * Method to pause current video
+   */
+  public pauseVideo() {
+    this.ready ? this.player.pauseVideo() : (() => { })();
+  }
+}
